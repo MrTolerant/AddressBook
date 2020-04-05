869205e0762fc139f87bb574399c16988652d7c3
18274774 <pvllebedev@sberbank.ru>
2020-04-05 16:35:47 +0300

    .

---
+++ server/db.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose
+  .connect(process.env.dbURL || 'mongodb://localhost:27017/meter', {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('Connected to database.'))
+  .catch(() => {
+    console.log('Cannot connect to database. Exiting.')
+    process.exit()
+  })
